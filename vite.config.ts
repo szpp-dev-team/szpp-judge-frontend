2393d5bcea9787b87b70b1d54918e9572a465ecc
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2022-10-25 01:55:33 +0900

    chore: パッケージ名のエイリアスを設定した
    nuxt っぽくしたかったため。~/で解決できるようになった

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+// @ts-ignore // path だけのために @types/node を入れることで node のバージョンに気をつけないといけなくなるのが億劫なので。
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,6 +9,12 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // @ts-ignore
+      '~/': `${path.resolve(__dirname, 'src')}/` // 末尾の/必須
+    }
+  },
   plugins: [
     vue(),
     AutoImport({
