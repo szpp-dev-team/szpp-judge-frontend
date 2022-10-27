7b0d55ffdc88f53c0fffedb745b51dc804a66d1e
kaji <kajiharalivi@icloud.com>
2022-10-28 00:53:21 +0900

    feat(pages): rankingの修正 おそらくできました。たぶん

--- src/pages/contests/ranking/ranking.vue
+++ src/pages/contests/ranking/ranking.vue
@@ -3,45 +3,66 @@
     <h1 class="center">ランキング</h1>
     <div class="center">
       <el-table
-        :data="results"
+        :data="Ranking.rankInfoList"
         stripe
         :border="true"
         style="width: 80%"
         :header-cell-style="{ textAlign: 'center' }"
         :cell-style="{ padding: '3px', height: '50px' }"
       >
-        <el-table-column label="順位" width="80" class-name="score">
+        <el-table-column label="順位" width="80">
           <template #default="scope">
             <div class="center">
               {{ scope.row.rank }}
             </div>
           </template>
         </el-table-column>
-        <el-table-column
-          prop="name"
-          label="ユーザ"
-          label-class-name="problem"
-          width="200"
-        />
-        <el-table-column label="得点" width="100">
+        <el-table-column label="ユーザ" width="200">
           <template #default="scope">
             <div class="center">
-              {{ scope.row.score }}
+              {{ scope.row.username }}
             </div>
           </template>
         </el-table-column>
+        <el-table-column label="得点" width="100">
+          <template #default="scope">
+            <div class="center">
+              <p class="rankScore">
+                {{ scope.row.score }}
+              </p>
+              <p class="penarty" v-if="scope.row.penartyCount > 0">
+                ({{ scope.row.penartyCount }})
+              </p>
+            </div>
+            <p class="duration" v-if="scope.row.score != 0">
+              {{ timeParse(scope.row.duration) }}
+            </p>
+          </template>
+        </el-table-column>
         <el-table-column
-          v-for="(prob, index) in 12"
+          v-for="(info, index) in rankInfo.taskInfoList"
           :key="index"
-          :label="`${prob}`"
+          :label="`${info.taskId}`"
           width="100"
         >
-          <template #default="scope">
-            <p style="margin: 0">
-              {{ scope.row.pro[0] }}
-            </p>
-            <p style="margin: 0">
-              {{ scope.row.pro[1] }}
+          <template #default>
+            <div class="center">
+              <p
+                class="penarty"
+                v-if="info.score == 0 && info.penartyCount != 0"
+              >
+                ({{ info.penartyCount }})
+              </p>
+              <p v-else-if="info.score == 0 && info.penartyCount == 0">-</p>
+              <p class="taskScore" v-else>
+                {{ info.score }}
+                <span class="penarty" v-if="info.penartyCount != 0">
+                  ({{ info.penartyCount }})
+                </span>
+              </p>
+            </div>
+            <p class="duration" v-if="info.score != 0">
+              {{ timeParse(info.duration) }}
             </p>
           </template>
         </el-table-column>
@@ -52,26 +73,89 @@
 <script setup lang="ts">
 import useAuthStore from '~/stores/authStore'
 import { storeToRefs } from 'pinia'
-import { RowJustify } from 'element-plus'
+import { ref } from '@vue/reactivity'
+import { RankInfo, RankingResponse, TaskInfo } from '~/model/ranking'
 const auth = useAuthStore()
 const { user } = storeToRefs(auth)
 
-//test
-const problems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const rankingInfo = ref<RankingResponse[]>([])
 
-const results = [
-  {
-    rank: '1',
-    name: 'hoge',
-    score: '23423',
-    pro: ['123', '121212']
-  },
-  { rank: '2', name: 'hoge', score: '1231', pro: ['100', '121212'] },
-  { rank: '3', name: 'hoge', score: '1231', pro: ['123', '121212'] },
-  { rank: '4', name: 'hoge', score: '1231', pro: ['123', '121212'] },
-  { rank: '5', name: 'hoge', score: '1231', pro: ['123', '121212'] },
-  { rank: '6', name: 'hoge', score: '1231', pro: ['123', '121212'] }
-]
+//test 秒
+
+const timeParse = (duration: number): string => {
+  const min = Math.floor(duration / 60)
+  const sec = duration % 60
+  return String(min) + ':' + String(sec)
+}
+
+const taskInfo: TaskInfo = {
+  taskId: 1,
+  score: 0,
+  duration: 1111,
+  penartyCount: 100,
+  submitIds: []
+}
+const taskInfo2: TaskInfo = {
+  taskId: 2,
+  score: 100,
+  duration: 234,
+  penartyCount: 100,
+  submitIds: []
+}
+const taskInfo3: TaskInfo = {
+  taskId: 3,
+  score: 0,
+  duration: 0,
+  penartyCount: 0,
+  submitIds: []
+}
+const taskInfo4: TaskInfo = {
+  taskId: 3,
+  score: 100,
+  duration: 0,
+  penartyCount: 100,
+  submitIds: []
+}
+
+const rankInfo: RankInfo = {
+  rank: 1,
+  userId: 11,
+  username: 'kajikaji',
+  score: 100,
+  duration: 111,
+  penartyCount: 100,
+  taskInfoList: [taskInfo, taskInfo2, taskInfo3, taskInfo4]
+}
+
+const rankInfo2: RankInfo = {
+  rank: 2,
+  userId: 11,
+  username: 'tsumagari',
+  score: 100,
+  duration: 0,
+  penartyCount: 100,
+  taskInfoList: [taskInfo, taskInfo2, taskInfo3]
+}
+
+const Ranking: RankingResponse = {
+  rankInfoList: [rankInfo, rankInfo2]
+}
+
+// const problems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+// const results = [
+//   {
+//     rank: '1',
+//     name: 'hoge',
+//     score: '23423',
+//     pro: ['123', '121212']
+//   },
+//   { rank: '2', name: 'hoge', score: '1231', pro: ['100', '121212'] },
+//   { rank: '3', name: 'hoge', score: '1231', pro: ['123', '121212'] },
+//   { rank: '4', name: 'hoge', score: '1231', pro: ['123', '121212'] },
+//   { rank: '5', name: 'hoge', score: '1231', pro: ['123', '121212'] },
+//   { rank: '6', name: 'hoge', score: '1231', pro: ['123', '121212'] }
+// ]
 </script>
 
 <style lang="scss" scoped>
@@ -109,13 +193,27 @@ const results = [
   background-color: #f0ad4e;
   color: white;
 }
-</style>
 
-<!-- <style>
-.score {
+.rankScore {
+  margin: 0;
+  color: blue;
+  font-weight: bold;
+}
+
+.taskScore {
+  margin: 0;
+  color: #5cb85c;
+  font-weight: bold;
+}
+.penarty {
+  margin: 0;
+  color: red;
+  font-weight: normal;
+}
+.duration {
+  color: #888;
   display: flex;
-  justify-content: space-around;
-  text-align: center;
+  justify-content: center;
   margin: 0;
 }
-</style> -->
+</style>
