d856cc9c47ef6acf7afabc82260c3824ba687c60
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2022-10-27 17:35:43 +0900

    refactor: markdown-it app.use でグローバルに登録するのをやめた
    コンポーネント単位で import すればよい

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
-import MarkDown from 'vue3-markdown-it'
 
 import './styles/index.scss'
 
-createApp(App).use(router).use(createPinia()).use(MarkDown).mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app')
