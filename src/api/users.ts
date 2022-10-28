844fe67e8a141439321869d778534de4a72a3f5e
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2022-10-29 03:20:04 +0900

    feat(apis): users (ユーザ登録用)を実装した

---
+++ src/api/users.ts
@@ -0,0 +1,12 @@
+import { httpGet, httpPost } from '~/utils/axiosClient'
+import { UserPayload, UserResponse } from '../model/users'
+
+export const register = async (payload: UserPayload): Promise<UserResponse> => {
+  const resp = await httpPost<UserResponse>('/users', payload)
+  return resp.data
+}
+
+export const getUser = async (userId: string): Promise<UserResponse> => {
+  const resp = await httpGet<UserResponse>(`/users/${userId}`)
+  return resp.data
+}
